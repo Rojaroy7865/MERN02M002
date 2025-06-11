29fca9a921de11125ddf0659cbca5e56f5efef2a
Rojaroy7865 <royroja9876@gmail.com>
2025-06-11 20:42:38 +0530

    Your commit message here

---
+++ script.js
@@ -0,0 +1,102 @@
+// cons0ole.log("welcome ")
+// let-="quilth"
+// consloe.log($)
+
+// let 1=2
+
+// let name="pagal"
+// consloe.log($)
+
+// let nameofStudent ="Rudraditya"
+// console.log(nameofStudent)
+
+// let isActive = true;
+
+// let a=20:
+// console.log(a)
+// console.log(typeof a)
+
+// let name="Quilth"
+// console.log(name,)
+
+// let
+//  let b;
+//  consloe.log(b);
+
+//  let c=30;
+//  console.log(c)
+//  console.log(typeof  typeofc)
+
+//  let d=30.5;
+//  console.log(typeof d)
+
+ 
+//  { 
+//     console.log 
+//  }
+//  function getName()
+//  {
+//     console log(full name)
+
+//  }
+//  {
+//     var a=40;
+//     console.log(a)
+
+//  }
+//  console.log (a)
+//  function get a ()
+//  console.log ("function get declaretion" A)
+
+// }
+// function variableDeclaRE()
+// VAR R=50;
+// console.log(r)
+// let name=("Innovation ")
+
+
+// //block lavle declaration 
+// {let a=30
+// console.log(a)
+// } 
+// console.log(local access:",a")
+
+// function geta(){
+//     console.log("function level declaration ")
+// }
+
+// const name="xyz"
+// console.log(name)
+
+// {
+//     console.log(name)
+
+// }
+// function getName(){
+//     console.log(name)
+// }
+// {
+//     const a=70:
+// console.log(a)
+// }
+// var a=60;
+// var a=30;
+// console.log(a)
+
+// let a=60;
+// console.log(a)
+// let a=80:
+// console.log(a)
+
+// const s="string",
+// console.log(s)
+// const.s="string"
+
+let declaretion 
+let a;
+console.log(a)
+
+
+console.log6+
+1
+4pppp0;
---
+++ week.2/day2/loop.js
@@ -0,0 +1,120 @@
+console.log("hellow Quilth Inoviation")
+
+console.log(1 !=="1");
+
+//let is="true" 
+console.log(true !==true);
+
+console.log('10''-4''-3'-2'+'5');
+
+    let a=3;
+    let b=a++;
+    console.log(a,b);
+
+    console.log(0== '');
+    console.log(0== '');
+      
+    console.log(!!false);
+    console.log(!!"flase");
+
+    //loop
+    let age= prompt("enter you age ")
+    if (age>=18){
+        console.log(he/she can eliginble for voting );
+    }
+    else{
+        console.log(he/she can not eligiblle for vote);
+    }
+     let stMark=40;
+     if(stMark>=90){
+        console.log("grade a")
+
+     }
+     else if (stMark>=80){
+        console.log(grade b);
+     }elseif (stMark>=30){
+        console.log(geade c);
+     }
+
+
+
+     let stage=40;
+     console.log(stage>=?"can vote":"can't vote");
+
+     
+
+     let number=prumpt ("entee a number "):
+     if (num%==5):
+     {
+    console.log(num, number is divisible  by 5");
+
+     }else {
+
+     }
+
+
+
+
+     //for loops
+     //for loop
+     //while 
+     //do while
+     
+
+     //for loops 
+      for(let i=1;i<=10;i++){
+        console.log(i);
+      }
+      //while 
+      let i=1;
+      while(i<=5){
+        console.log(i)
+        i++
+      }
+      let n=1
+      do{
+        console.log(n);
+      }while  (n<=15);
+      //
+      let obj={
+        name:"roja,
+        age:30,
+        phone:9937216245
+
+      }
+      console.log(obj.name);
+      console.log(obj.age);
+      console.log(obj.pho\e )
+      obj.name="y"
+      console.log(obj.name);
+
+
+      let arr=[1,2,3,4,5,6,7]
+      console.log(typeof arr);
+
+
+      for(const key in obj){
+        console.log(obj[key])
+      }
+      //or i use also
+      for( key in obj){
+        console.log(obj[key])
+      }
+      for(ley idx in arr)
+        console.log(arr[inx])
+    //forn of
+    for (conste of arr)
+        console.log(e)
+    //ubdation loops
+    //string 
+
+    let str ="quilth Inovation"
+    console.log("length is:"m,str.length);
+    console.log(str[0]);
+
+    str[0]="D";
+    console.log(str)
+    //interpollination string
+    let c=20;
+    console.log("value is:",c+)
+    console.log('value is:$(c)');
